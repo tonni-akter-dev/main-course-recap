ec6a6798209604df9f35d3243c6129eabf97c4e3
tonni-akter-dev <tanniakhter123@gmail.com>
2023-09-15 20:02:16 +0600

    kjdhkjdh

--- Milestone-10-Firebase/firebase-authentication-practice/src/components/Login/Login.jsx
+++ Milestone-10-Firebase/firebase-authentication-practice/src/components/Login/Login.jsx
@@ -1,9 +1,25 @@
 import React, { memo } from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../../firebase/firebase.init";
 
 const Login = memo(() => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user)
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
     <div>
-      <button>Google login</button>
+      <button onClick={handleGoogleSignIn}>Google login</button>
     </div>
   );
 });
